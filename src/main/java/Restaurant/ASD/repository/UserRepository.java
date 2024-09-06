8d44d4ebf5f4b9b4085461976e163b47d88cf8fe
leehwan0218 <thhoney218@gmail.com>
2024-09-06 10:10:33 +1000

    Change User to Products(complete)

--- src/main/java/Restaurant/ASD/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package Restaurant.ASD.repository;
-
-import Restaurant.ASD.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-
-}
