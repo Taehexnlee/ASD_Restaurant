ac744f4086f9c1f1e442ef7355d5e78a75937f75
TriumphantObituary <148290564+TriumphantObituary@users.noreply.github.com>
2024-10-27 22:45:16 +1100

    Add controllers + repositories

---
+++ ASD_azure_back/src/main/java/Restaurant/ASD/repository/OrderRepository.java
@@ -0,0 +1,8 @@
+package Restaurant.ASD.repository;
+
+import Restaurant.ASD.model.Order;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order,Long> {
+}
